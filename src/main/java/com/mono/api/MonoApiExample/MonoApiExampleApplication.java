d6fc1bb25466e8d49d5a4e180caac36b166509da
zevlavy <yevhen.valkovskyi@ericsson.com>
2021-03-10 17:41:06 +0200

    initial

---
+++ src/main/java/com/mono/api/MonoApiExample/MonoApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mono.api.MonoApiExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoApiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonoApiExampleApplication.class, args);
+	}
+
+}
